fbcda7e8ff49bdca29d7f5d0aedad020c3c91215
modtomdev <t.modolo.dev@gmail.com>
2025-12-04 17:23:36 +0100

    changed us to eu

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
+  availability_zone       = "eu-west-1a"
   map_public_ip_on_launch = true
   tags                    = { Name = "public-subnet" }
 }
@@ -16,7 +16,7 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = "eu-west-1a"
   tags              = { Name = "private-subnet" }
 }
 
