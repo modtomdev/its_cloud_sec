8525c7350cff66a5202bbb8748ff4a4640898e07
modtomdev <t.modolo.dev@gmail.com>
2026-01-09 09:23:26 +0100

    cors restriction

--- app/backend/main.py
+++ app/backend/main.py
@@ -7,9 +7,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    "http://frontend",
-    "*"
-]
+    "http://frontend"]
 
 app.add_middleware(
     CORSMiddleware,
